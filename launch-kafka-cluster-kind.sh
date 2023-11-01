5c391698c814dcf986bcdbce9011bd53239196e8
andonokar <anderson.lima@domrock.ai>
2023-11-01 15:24:50 -0300

    first git commit

--- launch-kafka-cluster-kind.sh
+++ launch-kafka-cluster-kind.sh
@@ -2,38 +2,38 @@
 sudo kind create cluster --config kind/kind.yaml
 
 # Creating namespaces
-kubectl create namespace kafka
+sudo kubectl create namespace kafka
 #kubectl create namespace monitoring
-kubectl create namespace spark-operator
+sudo kubectl create namespace spark-operator
 #kubectl create namespace mongodb-database
-kubectl create namespace airflow
+sudo kubectl create namespace airflow
 
 # Set airflow configmap volumes
 #kubectl apply -f airflow/requirements-configmap.yaml
 
 # Set docker and aws credentials
-kubectl apply -f ../kind-kafka/secrets/secret-docker.yaml
+sudo kubectl apply -f ../kind-kafka/secrets/secret-docker.yaml
 #kubectl apply -f secrets/secret-aws.yaml
 
 # Installing Strimzi
-helm repo add strimzi https://strimzi.io/charts/
-helm install strimzi strimzi/strimzi-kafka-operator -f strimzi/strimzi-values.yaml -n kafka
+sudo helm repo add strimzi https://strimzi.io/charts/
+sudo helm install strimzi strimzi/strimzi-kafka-operator -f strimzi/strimzi-values.yaml -n kafka
 
 # Give a time to install Strimzi
-sleep 120
+sudo sleep 120
 
 # Install mongo-operator
 #helm repo add mongodb https://mongodb.github.io/helm-charts
 #helm install mongodb mongodb/community-operator -f mongo/mongo-operator-values.yaml -n mongodb-database
 
 # Installing spark-operator
-helm repo add spark-operator https://googlecloudplatform.github.io/spark-on-k8s-operator
-helm install spark-operator spark-operator/spark-operator -f spark/spark-operator-values.yaml -n spark-operator
+sudo helm repo add spark-operator https://googlecloudplatform.github.io/spark-on-k8s-operator
+sudo helm install spark-operator spark-operator/spark-operator -f spark/spark-operator-values.yaml -n spark-operator
 
 # Deploying airflow
 #helm install airflow oci://registry-1.docker.io/bitnamicharts/airflow -n airflow -f airflow/airflow.yaml
-helm repo add apache-airflow https://airflow.apache.org
-helm upgrade --install airflow apache-airflow/airflow --namespace airflow -f airflow/airflow.yaml
+sudo helm repo add apache-airflow https://airflow.apache.org
+sudo helm upgrade --install airflow apache-airflow/airflow --namespace airflow -f airflow/airflow.yaml
 
 # Installing prometheus
 #helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
@@ -44,8 +44,8 @@ helm upgrade --install airflow apache-airflow/airflow --namespace airflow -f air
 #kubectl apply -f mongo/mongo-service.yaml
 
 # Deploying Kafka and zookeper for Strimzi
-kubectl apply -f strimzi/kafka-deploy.yaml -n kafka
-kubectl wait kafka/my-cluster --for=condition=Ready --timeout=600s -n kafka
+sudo kubectl apply -f strimzi/kafka-deploy.yaml -n kafka
+sudo kubectl wait kafka/my-cluster --for=condition=Ready --timeout=600s -n kafka
 
 # Installing kafka-exporter for prometheus monitoring
 #helm install kafka-exporter prometheus-community/prometheus-kafka-exporter -f strimzi/kafka-exporter-values.yaml -n kafka
@@ -57,6 +57,6 @@ kubectl wait kafka/my-cluster --for=condition=Ready --timeout=600s -n kafka
 #kubectl apply -f ksqldb/ksqldb-deploy.yaml
 
 # define permissions
-kubectl create clusterrolebinding default-pod --clusterrole cluster-admin --serviceaccount=spark-operator:default
-kubectl create clusterrolebinding default-pod2 --clusterrole cluster-admin --serviceaccount=airflow:default
-kubectl create clusterrolebinding default-pod3 --clusterrole cluster-admin --serviceaccount=airflow:airflow-worker
+sudo kubectl create clusterrolebinding default-pod --clusterrole cluster-admin --serviceaccount=spark-operator:default
+sudo kubectl create clusterrolebinding default-pod2 --clusterrole cluster-admin --serviceaccount=airflow:default
+sudo kubectl create clusterrolebinding default-pod3 --clusterrole cluster-admin --serviceaccount=airflow:airflow-worker
