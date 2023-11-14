9e8cbd7759ccd5e63b262358bbec9ad00faf3cac
andonokar <anderson.lima@domrock.ai>
2023-11-14 10:13:24 -0300

    changes por airflow connections

--- launch-kafka-cluster-kind.sh
+++ launch-kafka-cluster-kind.sh
@@ -13,22 +13,23 @@ sudo kubectl create namespace airflow
 
 # Set docker and aws credentials
 sudo kubectl apply -f ../kind-kafka/secrets/secret-docker.yaml
+sudo kubectl apply -f airflow/kafka-secret.yaml
 #kubectl apply -f secrets/secret-aws.yaml
 
 # Installing Strimzi
-sudo helm repo add strimzi https://strimzi.io/charts/
-sudo helm install strimzi strimzi/strimzi-kafka-operator -f strimzi/strimzi-values.yaml -n kafka
+#sudo helm repo add strimzi https://strimzi.io/charts/
+#sudo helm install strimzi strimzi/strimzi-kafka-operator -f strimzi/strimzi-values.yaml -n kafka
 
 # Give a time to install Strimzi
-sudo sleep 120
+#sudo sleep 120
 
 # Install mongo-operator
 #helm repo add mongodb https://mongodb.github.io/helm-charts
 #helm install mongodb mongodb/community-operator -f mongo/mongo-operator-values.yaml -n mongodb-database
 
 # Installing spark-operator
-sudo helm repo add spark-operator https://googlecloudplatform.github.io/spark-on-k8s-operator
-sudo helm install spark-operator spark-operator/spark-operator -f spark/spark-operator-values.yaml -n spark-operator
+#sudo helm repo add spark-operator https://googlecloudplatform.github.io/spark-on-k8s-operator
+#sudo helm install spark-operator spark-operator/spark-operator -f spark/spark-operator-values.yaml -n spark-operator
 
 # Deploying airflow
 #helm install airflow oci://registry-1.docker.io/bitnamicharts/airflow -n airflow -f airflow/airflow.yaml
@@ -44,8 +45,8 @@ sudo helm upgrade --install airflow apache-airflow/airflow --namespace airflow -
 #kubectl apply -f mongo/mongo-service.yaml
 
 # Deploying Kafka and zookeper for Strimzi
-sudo kubectl apply -f strimzi/kafka-deploy.yaml -n kafka
-sudo kubectl wait kafka/my-cluster --for=condition=Ready --timeout=600s -n kafka
+#sudo kubectl apply -f strimzi/kafka-deploy.yaml -n kafka
+#sudo kubectl wait kafka/my-cluster --for=condition=Ready --timeout=600s -n kafka
 
 # Installing kafka-exporter for prometheus monitoring
 #helm install kafka-exporter prometheus-community/prometheus-kafka-exporter -f strimzi/kafka-exporter-values.yaml -n kafka
